75223a48134152619de68388baa2bcf3e9309095
wangrui <ruiwang5913@163.com>
2023-08-03 17:04:04 +0800

    vocab建立

--- Dive_into_deep_learning/d2l/__init__.py
+++ Dive_into_deep_learning/d2l/__init__.py
@@ -5,9 +5,8 @@ Please import d2l by one of the following ways:
 from d2l import mxnet as d2l  # Use MXNet as the backend
 from d2l import torch as d2l  # Use PyTorch as the backend
 from d2l import tensorflow as d2l  # Use TensorFlow as the backend
-from d2l import paddle as d2l  # Use PaddlePaddle as the backend
+from d2l import jax as d2l  # Use Jax as the backend
 
 """
 
-__version__ = "0.17.6"
-__all__ = ['tensorflow', 'torch']
+__version__ = "1.0.0-beta0"
